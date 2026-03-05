4b197d026b5e9579ff8f6ac8db443b9f36cd49eb
Benjamin Pilgrim <benjamin.pilgrim@risqcapital.com>
2026-03-05 16:59:05 +0000

    Cleanup and reword

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,34 @@
             <b><a href="https://risqcapital.com">RISQ Capital</a></b> - Senior DevOps Engineer
           </header>
           <ul>
-            <li>Spearheaded delivery of a DevOps Strategy focused around five key initiatives; Foundational Infrastructure, Platform, Release Engineering, Security & Compliance and Site Reliability Engineering</li>
-            <li>Developed a AWS well-architected multi-account landing zone using Terraform and AWS CloudFormation, Organizations, CloudTrail and Config</li>
-            <li>Ensured people, pipelines and machines can securely access AWS APIs in a least-privileged manner using IAM, Identity Center, Systems Manager and Cognito</li>
-            <li>Designed and implemented a global hybrid network built around AWS Cloud WAN, connecting VPCs in multiple regions, as well as co-located networks</li>
-            <li>Facilitated rapid iteration and repeatable delivery through development of a python-based CI/CD toolchain that can be ran locally for rapid development iteration and in GitHub Actions for safe production deployments</li>
-            <li>Utilised AWS Systems Manager to provision and manage long-running tasks in performance-critical systems, on EC2 and managed hybrid instances and standardised architecture for scheduled and event driven tasks using containers, EventBridge, Step Functions and ECS</li>
-            <li>Reduced monthly AWS spend by 5% through targeted cost optimizations</li>
+            <li>
+              Spearheaded delivery of a DevOps Strategy focused around five key initiatives;
+              Foundational Infrastructure, Platform Engineering, Release Engineering, Security &
+              Compliance and Reliability Engineering
+            </li>
+            <li>
+              Developed a custom well-architected multi-account AWS Landing Zone
+            </li>
+            <li>
+              Ensured people, pipelines and machines can securely access AWS APIs in a
+              least-privileged manner using IAM, Identity Center, Systems Manager and Cognito
+            </li>
+            <li>
+              Designed and implemented a global hybrid network built around AWS Cloud WAN,
+              connecting VPCs in multiple regions, as well as co-located networks
+            </li>
+            <li>
+              Facilitated rapid iteration and repeatable delivery through development of a
+              python-based CI/CD/CT framework that can be ran locally for rapid development iteration
+              and in GitHub Actions for safe production deployments
+            </li>
+            <li>
+              Utilised AWS Systems Manager to provision and manage long-running tasks in
+              performance-critical systems, on EC2 and managed hybrid instances and standardised
+              architecture for scheduled and event driven tasks using containers, EventBridge, Step
+              Functions and ECS
+            </li>
+            <li>Reduced monthly AWS spend by 10% through targeted cost optimizations</li>
           </ul>
         </main>
       </div>
@@ -30,37 +51,53 @@
             <b><a href="https://www.mmtdigital.co.uk/">MMT Digital</a></b> - Cloud Systems Engineer
           </header>
           <header>
-            <b><a href="https://careers.vodafone.co.uk/digital-consumer">Vodafone</a></b> - Senior DevOps
-            Engineer <span style="opacity: 0.75">(via MMT Digital)</span>
+            <b><a href="https://careers.vodafone.co.uk/digital-consumer">Vodafone</a></b> - Senior
+            DevOps Engineer <span style="opacity: 0.75">(via MMT Digital)</span>
           </header>
           <ul>
             <li>
-              Helped to define and implement a strategy for the creation of a global Internal Developer Platform, driving internal efficiency while reducing time-to-market
+              Shortlisted as a finalist for
+              <a
+                href="https://event.computing.co.uk/digitaltechnologyleaderswards2023/en/page/2023-finalists"
+                >Young Digital Professional of the Year</a
+              >
+              in the 2023 Computing Digital Technology Leaders Awards
             </li>
             <li>
-              Delivered an end-to-end proof of concept, successfully onboarding our first local market team, demonstrating value to the wider business and providing rapid feedback to component teams
+              Helped to define and implement a strategy for a global Internal Developer Platform,
+              driving efficiency while reducing time-to-market for new functionality
             </li>
             <li>
-              Designed and lead development of serverless control plane APIs for the platform, using AWS Lambda, API Gateway, Step Functions, DynamoDB and S3
-            </li>
-            <li>Maintained Terraform modules for Lambda, API Gateway and CloudFront used by multiple teams across Vodafone</li>
-            <li>
-              Worked across team boundaries, delegating where necessary, to mitigate blockers to strategic delivery and ensure the platform was delivered on time and to a high standard
-            </li>
-            <li>
-              Lead the design and development of an Azure Devops based CI/CD/CT framework, enabling teams to deploy their services to multiple environments with minimal configuration
+              Participated in small rapid delivery sprints of key engineers selected to mitigate
+              strategic blockers, including integrating and onboarding the first client team,
+              demonstrating value to the wider business and providing feedback to downstream
+              development teams
             </li>
             <li>
-              Developed a CLI tool and web interface for parameterised service templates, enabling deployment of new services, complete with baseline CI/CD, infrastructure and code in minutes rather than days
+              Designed and lead development of control plane APIs for the platform, leveraging
+              serverless technologies like AWS Lambda, API Gateway, Step Functions, DynamoDB and S3
             </li>
             <li>
-              Guided my team to improve code quality and reliability through automated testing, linting, formatting and type-checking at CI time
+              Lead the design and development of an Azure Devops based CI/CD/CT framework, enabling
+              teams to build and deploy services to multiple environments with minimal configuration
             </li>
             <li>
-              Mentored junior and mid level engineers through pair programming, code reviews, helping to improve their
-              technical skills and confidence
+              Developed a CLI tool and web interface for parameterised service templates, enabling
+              deployment of new services, complete with baseline CI/CD, infrastructure and code in
+              minutes rather than days
             </li>
-            <li>Shortlisted as a finalist for <a href="https://event.computing.co.uk/digitaltechnologyleaderswards2023/en/page/2023-finalists">Young Digital Professional of the Year</a> in the 2023 Computing Digital Technology Leaders Awards</li>
+            <li>
+              Maintained widely adopted internal Terraform modules for AWS infrastructure, ensuring
+              they are well-documented, tested and easy to use for downstream teams
+            </li>
+            <li>
+              Guided my team to improve code quality and reliability through automated testing,
+              linting, formatting and type-checking at CI time
+            </li>
+            <li>
+              Mentored junior and mid level engineers through pair programming and code reviews,
+              helping to improve their technical skills and confidence
+            </li>
           </ul>
         </main>
       </div>
@@ -71,16 +108,34 @@
             <b><a href="https://www.htk.co.uk/">HTK</a></b> - Software Engineer
           </header>
           <ul>
-            <li>Worked on a variety of services and projects, primarily as a Java backend engineer on an event-driven multi-tenant loyalty and marketing automation platform</li>
-            <li>Developed a real-time transaction and promotion rules engine using Spring Boot, Kafka, Storm and DocumentDB</li>
-            <li>Wrote T-SQL migration scripts and helped maintain schemas and stored procedures for multiple large databases</li>
             <li>
-              Developed a proof of concept for a multi-account AWS architecture using Terraform for a greenfield data lakehouse platform, driving my interest in cloud engineering
+              Worked as a Java <b>backend engineer</b> on an
+              <b>event-driven multi-tenant</b> loyalty and marketing automation <b>SaaS</b> platform
             </li>
             <li>
-              Designed and developed frontend user interface pages and components, including a
-              complex promotion builder with a 'drag-n-drop' nested condition editor for use by
-              customers
+              Developed a real-time transaction and promotion rules engine using Spring Boot,
+              <b>Kafka</b>, Storm and DocumentDB
+            </li>
+            <li>
+              Wrote T-<b>SQL</b> migration scripts and helped maintain schemas and stored procedures
+              for Microsoft SQL Server databases
+            </li>
+            <li>
+              Integrated with third party platforms, including PoS systems, and designed and
+              developed <b>ETL pipelines</b>
+              for ingesting customer transaction data
+            </li>
+            <li>
+              Developed a proof-of-concept <b>multi-account AWS landing zone</b> using
+              <b>Terraform</b> for a greenfield data lake-house platform
+            </li>
+            <li>
+              Integrated VueJS framework into a legacy Java Server Pages environment to enable
+              <b>incremental adoption</b> of modern frontend technologies
+            </li>
+            <li>
+              Designed and built frontend pages and components, including a nested condition builder
+              with a 'drag-n-drop' interface
             </li>
           </ul>
         </main>
@@ -95,15 +150,6 @@
         </main>
       </div>
     </section>
-<!--    <div class="row">-->
-<!--      <aside></aside>-->
-<!--      <main style="display: flex; align-items: center; justify-content: center;">-->
-<!--        <img alt="Cloud Essentials" style="width: 75px;" src="https://images.credly.com/size/340x340/images/ec621e2a-c8f0-4459-806c-ae11829d372a/image.png"/>-->
-<!--        <img alt="Architecting" style="width: 75px;" src="https://images.credly.com/size/340x340/images/519a6dba-f145-4c1a-85a2-1d173d6898d9/image.png"/>-->
-<!--        <img alt="Serverless" style="width: 75px;" src="https://images.credly.com/size/340x340/images/e07c6cc4-b737-4d7e-8ce8-66b6b7a60367/image.png"/>-->
-<!--        <img alt="Networking Core" style="width: 75px;" src="https://images.credly.com/size/340x340/images/e75f222b-7f75-4d7b-8a6a-67d68aa59d62/image.png"/>-->
-<!--      </main>-->
-<!--    </div>-->
   </div>
 </template>
 
